6cbd19a1681b0f2a903ce70951c3bab22c9a5a7f
Heribert Schütz <hs@webxcerpt.com>
2023-12-13 13:38:24 +0100

    removed the page-leaving confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -14,4 +14,8 @@ const App: FC = () => {
 
 export default App;
 
-window.onbeforeunload = () => confirm("Really leave this page?")
+// It can easily happen that you want to navigate within the slideshow
+// but you accidentially navigate away from it.  This can be annoying.
+// To avoid this, enable the confirmation question.
+// OTOH, the confirmation question can also be annoying...
+// window.onbeforeunload = () => confirm("Really leave this page?")
