5e6f78fd07adc2c5c66fd853d40b3498759ac3d7
Heribert Schütz <hs@webxcerpt.com>
2023-12-15 02:20:29 +0100

    explained problem with geodesics

--- src/slides/Buzzer.tsx
+++ src/slides/Buzzer.tsx
@@ -1,9 +1,26 @@
 import { ThreeEvent } from '@react-three/fiber';
+import { Line, Sphere } from '@react-three/drei';
 import { useControls } from 'leva';
-import { FC, useCallback, useMemo, useState } from 'react';
-import { DoubleSide, Vector3 } from 'three';
+import { FC, Fragment, useCallback, useMemo, useState } from 'react';
+import { DoubleSide, MeshStandardMaterial, Vector3 } from 'three';
 import { quadrangulate, roundedBoxSurfaceGenerator, triangulate } from '../SurfaceGeometry';
 import { TAU, subdivide } from '../utils/lib';
+
+
+const controls = {
+  wireframe: false,
+
+  nStepsCap: {label: "#steps cap", value: 20, min: 1, max: 40, step: 1},
+  useSinesCap: true,
+  capRoughness: {value: 0.65, min: 0, max: 1},
+  capMetalness: {value: 0.6, min: 0, max: 1},
+
+  showGeodesics: false,
+
+  nStepsBox: {label: "#steps box", value: 3, min: 1, max: 50, step: 1},
+  radius: {value: 0.04, min: 0, max: 0.4},
+  boxRoughness: {value: 0.2, min: 0, max: 1},
+};
 
 export const Buzzer : FC<{
   onPress  ?: (e: ThreeEvent<PointerEvent>) => void,
@@ -26,19 +43,9 @@ export const Buzzer : FC<{
   const {
     wireframe,
     nStepsCap, useSinesCap, capRoughness, capMetalness,
+    showGeodesics,
     nStepsBox, radius, boxRoughness
-  } = useControls("buzzer", {
-    wireframe: false,
-
-    nStepsCap: {label: "#steps cap", value: 20, min: 1, max: 40, step: 1},
-    useSinesCap: true,
-    capRoughness: {value: 0.65, min: 0, max: 1},
-    capMetalness: {value: 0.6, min: 0, max: 1},
-
-    nStepsBox: {label: "#steps box", value: 3, min: 1, max: 50, step: 1},
-    radius: {value: 0.04, min: 0, max: 0.4},
-    boxRoughness: {value: 0.2, min: 0, max: 1},
-  });
+  } = useControls("buzzer", controls);
 
   const boxRadii = new Vector3(1, 1, 1).multiplyScalar(radius);
 
@@ -49,37 +56,44 @@ export const Buzzer : FC<{
     }), [nStepsCap, useSinesCap]);
 
   return(<>
-    <mesh position={[0, 0, pressed ? 0.05  : 0.10]}
-      onPointerDown={press}
-      onPointerUp={release}
-      onPointerLeave={release}
-    >
-      <surfaceGeometry args={[({addVertex, addTriangle}) =>
-        [-1, 1].flatMap(signX =>
-          [-1, 1].flatMap(signY => {
-            const invert = signX * signY < 0;
-            triangulate(nStepsCap, {
-              addVertexFromIndices: (i, j, k) => {
-                const v = new Vector3(
-                  signX * capSteps[i],
-                  signY * capSteps[j],
-                  capSteps[k]
-                ).normalize();
-                return addVertex(v, {normal: v});
-              },
-              emitTriangle: (a, b, c) => addTriangle(a, b, c, {invert}),
-            });
-          })
-        )
-      ]} />
-      <meshStandardMaterial
-        color="red"
-        wireframe={wireframe}
-        metalness={capMetalness}
-        roughness={capRoughness}
-        side={DoubleSide}
-      />
-    </mesh>
+    <group position={[0, 0, pressed ? 0.05 : 0.10]}>
+      <mesh
+        onPointerDown={press}
+        onPointerUp={release}
+        onPointerLeave={release}
+      >
+        <surfaceGeometry args={[({addVertex, addTriangle}) =>
+          [-1, 1].flatMap(signX =>
+            [-1, 1].flatMap(signY => {
+              const invert = signX * signY < 0;
+              triangulate(nStepsCap, {
+                addVertexFromIndices: (i, j, k) => {
+                  const v = new Vector3(
+                    signX * capSteps[i],
+                    signY * capSteps[j],
+                    capSteps[k]
+                  ).normalize();
+                  return addVertex(v, {normal: v});
+                },
+                emitTriangle: (a, b, c) => addTriangle(a, b, c, {invert}),
+              });
+            })
+          )
+        ]} />
+        <meshStandardMaterial
+          color="red"
+          wireframe={wireframe}
+          metalness={capMetalness}
+          roughness={capRoughness}
+          side={DoubleSide}
+        />
+      </mesh>
+      {showGeodesics &&
+        <group rotation={[0, 0, TAU/2]}>
+          <Geodesics/>
+        </group>
+      }
+    </group>
     <mesh>
       <surfaceGeometry args={[roundedBoxSurfaceGenerator(
         new Vector3(-1, -1, -0.8).add(boxRadii),
@@ -99,3 +113,79 @@ export const Buzzer : FC<{
     </mesh>
   </>);
 };
+
+
+// ---------------------------------------------------------------------------
+// The following display of subdivided geodesics is for comparison with
+// https://prideout.net/blog/octasphere
+
+// <Line/>s take colors but <Sphere/>s take materials.
+const colors = ["cyan", "magenta", "yellow"];
+const materials = colors.map(color => new MeshStandardMaterial({color}));
+
+function interpolateSegments(w: number, points: Vector3[]): Vector3 {
+  const wn = w * (points.length - 1)
+  const idx = Math.floor(wn);
+  const lambda = wn - idx;
+  return lambda === 0
+  ? points[idx]
+  : points[idx].clone().multiplyScalar(1-lambda).add(
+      points[idx + 1].clone().multiplyScalar(lambda)
+    );
+}
+
+function geodesicPoints(from: Vector3, to: Vector3): Vector3[] {
+  const result: Vector3[] = [];
+
+  // Recursively bipartitioning the angle between a and b
+  function fill(a: Vector3, b: Vector3, depth: number) {
+    if (depth <= 0) return;
+    const mid = a.clone().add(b).multiplyScalar(0.5).normalize();
+    fill(a, mid, depth - 1);
+    result.push(mid);
+    fill(mid, b, depth - 1);
+  }
+
+  result.push(from);
+  fill(from, to, 5); // TODO don't hard-wire the depth
+  result.push(to);
+
+  return result;
+}
+
+function Geodesics() {
+  const {nStepsCap} = useControls("buzzer", controls);
+  return subdivide(0, 1, nStepsCap).flatMap((u, i) => {
+    const v = 1 - u;
+    const sinU = Math.sin(u * (TAU/4));
+    const sinV = Math.sin(v * (TAU/4)); // === Math.cos(u * (TAU/4))
+    const pointsA = geodesicPoints(
+      new Vector3(sinU, sinV, 0),
+      new Vector3(sinU, 0, sinV),
+    );
+    const pointsB = geodesicPoints(
+      new Vector3(0, sinU, sinV),
+      new Vector3(sinV, sinU, 0),
+    );
+    const pointsC = geodesicPoints(
+      new Vector3(sinV, 0, sinU),
+      new Vector3(0, sinV, sinU),
+    )
+    const pointss = [pointsA, pointsB, pointsC];
+    return pointss.map((points, j) => {
+      const color = colors[j]
+      return (
+        <Fragment key={`${i}-${j}`}>
+          <Line points={points} color={color}/>
+          {i < nStepsCap && subdivide(0, 1, nStepsCap - i).map((w, k) => (
+            <Sphere key={k}
+              material={materials[j]}
+              position={interpolateSegments(w, points)}
+              scale={0.01}
+            />
+          ))}
+        </Fragment>
+      );
+    });
+  })
+}
--- src/slides/BuzzerSlide.tsx
+++ src/slides/BuzzerSlide.tsx
@@ -9,7 +9,7 @@ import { TAU } from '../utils/lib';
 
 const schema = {
   scale: {label: "scale", value: 1.6, min: 0.1, max: 4},
-  xShift: {label: "x shift", value: 0.025, min: -1, max: 1},
+  xShift: {label: "x shift", value: 0.8, min: -1, max: 1},
   yShift: {label: "y shift", value: -0.1, min: -1, max: 1},
   zShift: {label: "z shift", value: 0, min: -1, max: 1},
 };
@@ -21,23 +21,90 @@ export const BuzzerSlide: FC = () => {
     <Slide>
       <div style={{
         zIndex: 1,
-        position: "absolute", left: 30, top: 0, width: "30%", fontSize: 24
+        position: "absolute", left: 30, top: 0, width: "42%", fontSize: 24,
+        backgroundColor: "#0028",
       }}>
         <h2>A Hemisphere and a Box</h2>
         <ul>
           <li>Play with the controls in the config box.</li>
           <li>Press the buzzer.</li>
         </ul>
-        <p>
-          Apparently I have re-invented what is called an "octasphere" elsewhere,
-          for example in <a href="https://prideout.net/blog/octasphere/">Philip
-          Rideout's blog</a>.
-          There is a very small difference in the placement
-          of the inner vertices of the triangle subdivisions
-          (and of course I think that my solution is more elegant ;-).
-          <br />
-          TODO: Elaborate a bit on this placement.
-        </p>
+        <div style={{fontSize: "50%"}}>
+          <p>
+            Apparently I have re-invented
+            what is called an "octasphere" elsewhere,
+            for example
+            {} <a href="https://prideout.net/blog/octasphere/">here</a>,
+            {} <a href="https://catlikecoding.com/unity/tutorials/procedural-meshes/octasphere/">here</a>,
+            and <a href="https://catlikecoding.com/unity/tutorials/procedural-meshes/geodesic-octasphere/">here</a>.
+          </p>
+          <p>
+            There is a subtle aspect regarding the placement of auxiliary vertices.
+            To see it,
+            check the "showGeodesics" toggle
+            in the config box
+            and set "#steps cap" to a smaller number, but at least 3.
+          </p>
+          <p>
+            The yellow vertices are computed by evenly subdividing the yellow
+            geodesics connecting corresponding points on the two evenly
+            subdivided meridians.
+            But one could, with equal justification, connect the equator with
+            one of the two meridians and get the cyan or magenta geodesics and
+            vertices,
+            which are not the same as the yellow ones.
+            (In a flat triangle the three vertex families would coincide.)
+          </p>
+          <p>
+            The approach chosen for the buzzer cap treats
+            the three coordinate axes and thus
+            the three directions of the spherical triangle equally.
+            Check the "wireframe" and "useSinesCap" toggles to see the triangulation.
+            Rotating the spherical triangle around its center by ±120°
+            maps the auxiliary vertices of the red wireframe
+            to auxiliary vertices again.
+            This is not true for any of the three geodesics-based approaches.
+          </p>
+          <p>
+            In detail:
+            Consider the triangular face of the unit octogon in the (+,+,+) octant
+            and assume some resolution <em>n ∈ ℕ</em>.
+            Then each auxiliary vertex on that face
+            has coordinates <em>(x, y, z)</em> such that
+            {} <em>x+y+z = 1</em> and <em>x</em>, <em>y</em>, and <em>z</em> are
+            integral multiples of <em>1/n</em>.
+            (As a side node, "<em>( x : y : z )</em>" are called the "normalized barycentric
+            coordinates" in that triangle.)
+            Now we first map the vertex
+            to <em>(sin(90°·x), sin(90°·y), sin(90°·z))</em> {}
+            and then we normalize that vector.
+          </p>
+          <p>
+            This maps the triangle sides (the "main" meridians and the equator)
+            to equispaced geodesics:
+            <br />
+            On these lines one of <em>x, y, z</em> is zero.
+            For example, on the equator <em>z = 0</em> {}
+            (if you chose the <em>z</em> axis pointing to a "pole")
+            and <em>(x, y, 0) = (x, 1-x, 0)</em> is mapped
+            to <em>(sin(90°·x), sin(90°·(1-x)), 0) = (sin(90°·x), cos(90°·x), 0)</em>,
+            which is already normalized.
+            The triangle sides are of course also geodesics.
+          </p>
+          <p>
+            But the inner vertices need the normalization step.
+            They are not constructed on geodesics and not equispaced.
+          </p>
+          <p>
+            If the resolution is a multiple of 3, the central point of
+            the octogon face <em>(1/3, 1/3, 1/3)</em> is one of the
+            auxiliary vertices.
+            My approach (and even the straight-forward normalization
+            of vertices on the octogon face)
+            maps it to the center of the spheric triangle
+            whereas the geodesic approaches don't.
+          </p>
+        </div>
       </div>
       <Canvas>
         <OrbitControls enableRotate={false /* leave rotation to PresentationControls */} />
